a819a157c0ac9bcb5759a2957395b02748d46aff
Ozge <email>
2024-06-25 08:37:02 +0300

    Second Commit

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("hello");
         //added
+
+        System.out.println("ozgecik this my branch");
     }
 }
